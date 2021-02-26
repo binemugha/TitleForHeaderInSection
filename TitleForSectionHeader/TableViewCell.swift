77d41f912eb605feb83ecd801fdb76b02534b715
Benjamin Inemugha <binemugha@gmail.com>
2021-02-26 09:43:16 +0100

    Setup files

---
+++ TitleForSectionHeader/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TitleForSectionHeader
+//
+//  Created by Benjamin Inemugha on 26/02/2021.
+//  Copyright © 2021 Techelope. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
